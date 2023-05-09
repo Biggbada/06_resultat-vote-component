e2089e993cca2b344b079a1bf067ab01e8003951
Biggbada <yguenot@polepo.fr>
2023-05-09 16:56:36 +0200

    feat/main : fin de projet
    projet ok

--- src/index.js
+++ src/index.js
@@ -1,25 +1,13 @@
-var requestURL = '/src/data.json';
-var request = new XMLHttpRequest();
-request.open('GET', requestURL);
-request.responseType = 'json';
-request.send();
+let reactionScore = document.getElementById('reaction-score');
+let memoryScore = document.getElementById('memory-score');
+let verbalScore = document.getElementById('verbal-score');
+let visualScore = document.getElementById('visual-score');
 
-
-
-console.log(requestURL);
-
-var score = JSON.parse(score)
-console.log(score);
-
-
-
-// request.onload = function() {
-//     var votes = request.response;
-//     populateHeader(votes);
-//     showHVotes(votes);
-// }
-
-// function showHVotes (jsonObj) {
-//     var reactionScore = document.getElementById('reaction-score');
-
-// }
+fetch("./data.json")
+    .then(data => data.json())
+    .then(votes => {
+        reactionScore.textContent = (votes[0].score);
+        memoryScore.textContent = (votes[1].score);
+        verbalScore.textContent = (votes[2].score);
+        visualScore.textContent = (votes[3].score);
+    })
